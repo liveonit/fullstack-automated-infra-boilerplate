be882c30de8ac40094ee03c27c191766a23d1b44
ibarretorey <ibarretorey@gmail.com>
2020-07-17 15:11:15 -0300

    add more vars to keyclaok realm

--- microservicios/dashboard/src/index.tsx
+++ microservicios/dashboard/src/index.tsx
@@ -6,7 +6,10 @@ import '@patternfly/react-core/dist/styles/base.css';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Root from './pages/Root';
+import { init } from './components/Keycloak'
 
+// Initialize the Keycloak client
+init();
 
 ReactDOM.render(<Root/>, document.getElementById('root'));
 
--- microservicios/dashboard/src/pages/Root.tsx
+++ microservicios/dashboard/src/pages/Root.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import history from "../utils/General/history";
 
+import Keycloak from "../components/Keycloak";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { client } from "../utils/General/GqlClient";
 import Layout from "../Layout";
@@ -9,23 +10,25 @@ import Home from "./Home";
 import DemoGrid from "./DemoGrid";
 import Page2 from "./Page2";
 import Page3 from "./Page3";
-import LogsPage from './Logs'
+import LogsPage from "./Logs";
 
 export function Root() {
   return (
-    <ApolloProvider client={client}>
-      <Router history={history}>
-        <Layout>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/demoGrid" component={DemoGrid} />
-            <Route path="/page2" component={Page2} />
-            <Route path="/page3" component={Page3} />
-            <Route path="/logs" component={LogsPage} />
-          </Switch>
-        </Layout>
-      </Router>
-    </ApolloProvider>
+    <Keycloak>
+      <ApolloProvider client={client}>
+        <Router history={history}>
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/demoGrid" component={DemoGrid} />
+              <Route path="/page2" component={Page2} />
+              <Route path="/page3" component={Page3} />
+              <Route path="/logs" component={LogsPage} />
+            </Switch>
+          </Layout>
+        </Router>
+      </ApolloProvider>
+    </Keycloak>
   );
 }
 
